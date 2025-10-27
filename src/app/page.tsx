a6991ce17dd6784c5e57394fddc9c2a58980e595
fmcnit <fmcnit@gmail.com>
2025-10-26 21:25:38 -0300

    feat: text footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -47,7 +47,7 @@ export default function Home() {
         />
       </main>
       <footer className="h-12 flex items-center justify-center text-red-600 ">
-        <p className="text-sm md:text-left w-full p-2">Site desenvolvido para atendimento do especialista Fabiano</p>
+        <p className="text-sm md:text-left w-full p-2">Site desenvolvido para atendimento do especialista Fabiano Magalhães da Verisure Brasil</p>
       </footer>
     </div>
   );
